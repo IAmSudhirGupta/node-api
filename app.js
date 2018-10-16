499a7c95214ba605a8ae2da1227f72c2d4f5fb47
Sudhir Gupta <sudhirhitit@gmail.com>
2018-10-16 22:38:33 +0530

    add .env file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var db = require('./db');
 
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
-
 module.exports = app;
