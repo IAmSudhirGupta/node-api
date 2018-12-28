2dac63679dbed503a13140be41116209a44f4381
Sudhir Gupta <sudhirhitit@gmail.com>
2018-12-28 19:08:44 +0530

    field mandaotry

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -28,6 +28,7 @@ router.post('/register', function(req, res) {
                 address: req.body.address
             },
             function(err, user) {
+                console.log(err);
                 if (err) return res.status(500).send("There was a problem registering the user.")
                     // create a token
                 var token = jwt.sign({ id: user._id }, config.secret, {
--- user/User.js
+++ user/User.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
     userName: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstName: { type: String, required: false },
+    lastName: { type: String, required: false },
     phone: { type: Number, required: true },
     email: { type: String, required: true },
     gender: { type: String, required: false },
     dob: { type: Date, required: false },
     country: { type: String, required: false },
     companyName: { type: String, required: false },
-    address: { type: String, required: true },
+    address: { type: String, required: false },
     password: { type: String, required: true }
 });
 mongoose.model('User', UserSchema);
